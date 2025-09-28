e0a61704166c37442f280fec061d436f9889da40
Martin Kloeckner <mjkloeckner@gmail.com>
2025-09-28 00:03:05 -0300

    add `__cplusplus` guards

--- include/timer.h
+++ include/timer.h
@@ -1,9 +1,17 @@
 #ifndef _TIMER_H_
 #define _TIMER_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 void timer_init(void);
 void timer_add_callback(void (*func_ptr)(void));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // _TIMER_H_
